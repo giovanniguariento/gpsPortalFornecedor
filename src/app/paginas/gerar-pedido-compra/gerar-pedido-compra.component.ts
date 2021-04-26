b56528930c8346a8b119986f3b6ba32fa870fad6
giovanniguariento <giovanniguariento97@gmail.com>
2021-04-26 08:12:45 -0300

    correcoes

--- src/app/paginas/gerar-pedido-compra/gerar-pedido-compra.component.ts
+++ src/app/paginas/gerar-pedido-compra/gerar-pedido-compra.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gerar-pedido-compra.component.scss']
 })
 export class GerarPedidoCompraComponent implements OnInit {
-  
+
   public centroCusto;
   public localEntrega;
   public fornecedor;
@@ -14,22 +14,19 @@ export class GerarPedidoCompraComponent implements OnInit {
   public valorUni = 10;
   public valorTotal;
   public quantidade;
-  public regioesCadastradas: any = {
-    'titulo': 'JSON x XML',
-   };
-  carrinho = false;
+  public carrinho = false;
 
-    accounts = [
-        { name: 'Adam', email: 'adam@email.com', age: 12, country: 'United States' },
-        { name: 'Samantha', email: 'samantha@email.com', age: 30, country: 'United States' },
-        { name: 'Amalie', email: 'amalie@email.com', age: 12, country: 'Argentina' },
-        { name: 'Estefanía', email: 'estefania@email.com', age: 21, country: 'Argentina' }
-    ];
+  accounts = [
+    { name: 'Adam', email: 'adam@email.com', age: 12, country: 'United States' },
+    { name: 'Samantha', email: 'samantha@email.com', age: 30, country: 'United States' },
+    { name: 'Amalie', email: 'amalie@email.com', age: 12, country: 'Argentina' },
+    { name: 'Estefanía', email: 'estefania@email.com', age: 21, country: 'Argentina' }
+  ];
 
-    produtosTemp = [
-      { name: 'Kalunga', filial: '03', produto : 'papel'},
-      { name: 'Americanas', filial: '06', produto : 'caneta'},
-      { name: 'Atacadao', filial: '03' ,produto : 'mesa'}
+  produtosTemp = [
+    { name: 'Kalunga', filial: '03', produto: 'papel' },
+    { name: 'Americanas', filial: '06', produto: 'caneta' },
+    { name: 'Atacadao', filial: '03', produto: 'mesa' }
   ];
 
   constructor() { }
@@ -37,16 +34,16 @@ export class GerarPedidoCompraComponent implements OnInit {
   ngOnInit() {
   }
 
-  produtos(){
-this.fornecedor = this.produto;
+  produtos() {
+    this.fornecedor = this.produto;
 
   }
 
-  public valorTotalConta(){
+  public valorTotalConta() {
     this.valorTotal = this.valorUni * this.quantidade;
   }
 
-  public cancelar(){
+  public cancelar() {
     this.fornecedor = null;
     this.produto = null;
   }
