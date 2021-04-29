2fcbe1591989f1def6e29230ffabf99d12903ba1
giovanniguariento <giovanniguariento97@gmail.com>
2021-04-29 19:08:53 -0300

    show menu arrumado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { MenuService } from './services/menu.service';
 import { Component } from '@angular/core';
+import { AuthService } from './auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,20 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'portalFornecedor';
+
+  isShowMenu = localStorage.getItem('userLogin');
+
     
   
-    constructor( private menuService : MenuService) { }
+    constructor( private menuService : MenuService , private authService : AuthService) { }
   
     ngOnInit() {
+
+      this.authService.getShowMenuControl()
+      .subscribe(
+        (data : any) => { this.isShowMenu = data; }
+      );
+      
     }
 
     public abrirMenu(){
